33bf494d3ae41c198346498ac38a9c393e77debd
g00414253 <g00414253@atu.ie>
2024-12-04 16:30:08 +0000

    Created the project for membership service

---
+++ src/main/java/ie/atu/CICD_memberShipApplication.java
@@ -0,0 +1,11 @@
+package ie.atu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CICD_MemberShipApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CICD_MemberShipApplication.class, args);
+    }
+}
