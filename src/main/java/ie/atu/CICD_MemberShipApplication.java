43cb5b625fea5d0e38df0cd5232767236308d4c7
g00414253 <g00414253@atu.ie>
2025-01-08 18:46:49 +0000

    stuff works now

--- src/main/java/ie/atu/CICD_MemberShipApplication.java
+++ src/main/java/ie/atu/CICD_MemberShipApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+
 @SpringBootApplication
 public class CICD_MemberShipApplication {
     public static void main(String[] args) {
